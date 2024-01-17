1d408d61e289beb60b737b153d53afda503d97ec
Dmitry Bogatov <serenity@kaction.cc>
2024-01-17 02:53:16 -0500

    feat: Parse and lint multiple files in different threads
    On my laptop it brings time to process all .nix files in nixpkgs repository
    from 32 seconds to around 20. I expected more speed-up given that most of the
    work is CPU-bound. Further investigation is necessary.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use anyhow;
+use std::sync::{Arc, Mutex};
 use tree_sitter as ts;
 use tree_sitter_nix as nix;
 
@@ -7,30 +8,75 @@ mod types;
 mod util;
 
 fn main() -> anyhow::Result<()> {
-    let language = nix::language();
-    let mut parser = ts::Parser::new();
-    parser.set_language(language)?;
-
     let mut workers = vec![];
     for x in lamentation::MODULES {
         workers.push((x.new)()?);
     }
 
-    for fname in std::env::args().skip(1) {
-        let content = std::fs::read(&fname)?;
-        // None is only returned on misuse or timeout.
-        let tree = parser.parse(&content, None).unwrap();
+    // Fold iterator into the vector so the data shared by the threads
+    // is just an index.
+    let args: Vec<_> = std::env::args().skip(1).collect();
 
-        for w in &workers {
-            for e in w.lament(&tree, &content) {
-                println!(
-                    "{}:{}:{}: {:?} {}",
-                    &fname, e.line, e.column, e.kind, e.message
-                );
+    let index = Arc::new(Mutex::new(0));
+    let errcode = Arc::new(Mutex::new(0)); // doubles as stdout mutex
+    let args = Arc::new(args);
+    let workers = Arc::new(workers);
+
+    let mut threads = vec![];
+    for _ in 0..8 {
+        let index = Arc::clone(&index);
+        let args = Arc::clone(&args);
+        let errcode = Arc::clone(&errcode);
+        let workers = Arc::clone(&workers);
+
+        let thread = std::thread::spawn(move || {
+            let language = nix::language();
+            let mut parser = ts::Parser::new();
+            parser.set_language(language).unwrap();
+
+            loop {
+                let mut index = index.lock().unwrap();
+                if *index == args.len() {
+                    break;
+                }
+                let fname = &args[*index];
+                *index += 1;
+                drop(index);
+
+                match std::fs::read(fname) {
+                    Err(e) => {
+                        let mut errcode = errcode.lock().unwrap();
+                        *errcode = 1;
+                        eprintln!("Failed to read `{}': {:?}", fname, e);
+                    }
+                    Ok(content) => {
+                        // None is only returned on misuse or timeout.
+                        let tree = parser.parse(&content, None).unwrap();
+
+                        let mut laments = vec![];
+                        for w in workers.iter() {
+                            laments.extend(w.lament(&tree, &content));
+                        }
+
+                        if laments.len() > 0 {
+                            let mut errcode = errcode.lock().unwrap();
+                            *errcode = 1;
+                            for e in laments {
+                                println!(
+                                    "{}:{}:{}: {:?} {}",
+                                    &fname, e.line, e.column, e.kind, e.message
+                                );
+                            }
+                        }
+                    }
+                }
             }
-        }
-
-        parser.reset();
+        });
+        threads.push(thread);
     }
+    for t in threads {
+        t.join().unwrap();
+    }
+
     Ok(())
 }
